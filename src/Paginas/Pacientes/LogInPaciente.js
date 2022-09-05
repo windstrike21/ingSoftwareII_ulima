6721074f21def6c3d23a2e4e47c7849c22876075
Jimbravo <jimbravo05@hotmail.com>
2022-09-04 21:19:57 -0500

    avance

--- src/Paginas/Pacientes/LogInPaciente.js
+++ src/Paginas/Pacientes/LogInPaciente.js
@@ -2,8 +2,17 @@ import React, {Component} from 'react';
 
 const LogInPaciente = () => {
   return (
-    <div>hola mundo</div>
-  )
+      <div className='coutainerPrincipal'>
+  <div className='containerSecundario'>
+    <div className='form-group'>
+      <label> Tipo documento</label>
+    </div>
+
+    
+  </div>
+
+      </div>
+  );
 }
 
 export default LogInPaciente
