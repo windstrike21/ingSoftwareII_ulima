de2c22877b88b76e39438d2a9f3821eebdd8003a
Jimbravo <jimbravo05@hotmail.com>
2022-09-04 22:38:05 -0500

    .

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return <nav className="navbar navbar-expand-lg navbar-light bg-light">
