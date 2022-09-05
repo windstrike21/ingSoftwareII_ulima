42850e15a57851e3b1224849d046260702da0982
Jimbravo <jimbravo05@hotmail.com>
2022-09-04 23:23:45 -0500

    .

--- src/Paginas/Pacientes/LogInPaciente.js
+++ src/Paginas/Pacientes/LogInPaciente.js
@@ -1,16 +1,44 @@
+import { render } from '@testing-library/react';
 import React, {Component} from 'react';
 
 const LogInPaciente = () => {
   return (
       <div className='coutainerPrincipal'>
-  <div className='containerSecundario'>
-    <div className='form-group'>
-      <label> Tipo documento</label>
-    </div>
+        <div className='containerSecundario'>
+          <div className='form-group'>
 
-    
-  </div>
+            <label> Tipo documento</label>
+            <br/>
+            <input type="text" className='form-control'/>
+            <br/>
 
+            <label> Nro de documento</label>
+            <br/>
+            <input type="text" className='form-control'/>
+            <br/>
+
+            <label> Contraseña </label>
+            <br/>
+            <input type="password" className='form-control'/>
+            <br/>
+
+            <label> Código Captcha </label>
+            <br/>
+
+            <label> Código Captcha </label>
+            <br/>
+            <input type="password" className='form-control'/>
+            <br/>
+
+            <botton className = "btn btn-primary"> Iniciar Sesión</botton>
+            <br/>
+            <label> ¿Olvidaste tu contraseña? </label>
+            <br/>
+
+            <botton className = "btn btn-primary"> Iniciar Sesión</botton>
+
+          </div>
+        </div>
       </div>
   );
 }
