4b252fe05847967a25cd06883a26d0d71e3f181a
Wazonga <rmiglio115@gmail.com>
2022-09-09 23:30:31 -0500

    Se lograron los modales, falta solo ajustarlos completamente a lo pedido (Color y Relleno)

--- src/Paginas/Personal/RegistroPorPersonal.js
+++ src/Paginas/Personal/RegistroPorPersonal.js
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
-import Modal1 from "../Personal/modals/modal_Registrar1";
-import Modal2 from "../Personal/modals/modal_Registrar2";
-import Modal3 from "../Personal/modals/modal_Delete";
-import Modal4 from "../Personal/Actualizar hoja filiacion";
+import Modal1 from "../Personal/modals/mor1";
+import Modal2 from "../Personal/modals/mor2";
+import Modal3 from "../Personal/modals/mor3";
+import Modal4 from "../Personal/modals/mor4";
 
 function Registro_Personal() {
-  const [modalOpen4, setModalOpen4] = useState(false);
-  const [modalOpen3, setModalOpen3] = useState(false);
-  const [modalOpen1, setModalOpen1] = useState(false);
-  const [modalOpen2, setModalOpen2] = useState(false);
 
   return (
     <div className="Registro_Personal mt-5">
@@ -18,30 +14,13 @@ function Registro_Personal() {
       </div>
       
       <div className="d-grid gap-2 col-6 mx-auto">
-      <div className="row mt-3">
-      <button
-        className="openModalBtn  btn btn-md btn-primary" role="dialog"
-        onClick={() => {
-          setModalOpen1(true);
-        }}
-      >
-       Registrar Paciente
-      </button>
-      {modalOpen1 && <Modal1 setOpenModal1={setModalOpen1} />}
+      <div className="row mt-3 mb-3">
+        <Modal1/>
       </div>
 
 
-      <div className="row mt-3">
-      <button
-        className="openModalBtn mb-3 btn btn-md btn-primary"
-        onClick={() => {
-          setModalOpen2(true);
-        }}
-      >
-      Siguiente 
-      </button>
-      
-      {modalOpen2 && <Modal2 setOpenModal2={setModalOpen2} />}
+      <div className="row mt-3 mb-3">
+        <Modal2/>
       </div>
       </div>
 
@@ -59,27 +38,10 @@ function Registro_Personal() {
           </div>
         </td>
         <td> 
-        <div className="container d-grid gap-2 "> 
-        <button
-            className = "openModalBtn btn btn-md btn-primary" 
-            onClick   = {() => {
-            setModalOpen4(true);
-            }}
-      >
-            Abrir modal 3 - Ver hoja de Afiliación
-            </button>
-             {modalOpen4 && <Modal4 setOpenModal4={setModalOpen4} />}
-            
-            
-            <button
-            className = "openModalBtn btn btn-md  btn-danger" 
-            onClick   = {() => {
-            setModalOpen3(true);
-            }}
-      >
-            Abrir modal 3 - Eliminar
-            </button>
-             {modalOpen3 && <Modal3 setOpenModal3={setModalOpen3} />}
+        <div className="container d-grid gap-2 ">
+
+            <Modal3/>
+            <Modal4/>
              </div> 
         </td>
       </tr>
---
+++ src/Paginas/Personal/modals/mor4.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import Hoja from '../Actualizar hoja filiacion';
+
+class Modal4 extends React.Component{
+  state={
+    abierto: false,
+  }
+
+  abrirModal=()=>{
+    this.setState({abierto: !this.state.abierto});
+  }
+
+  render(){
+
+    const modalStyles={
+      position: "absolute",
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)'
+    }
+    return(
+      <>
+      <div className="principal">
+        <div className="secundario">
+      <Button color="success" onClick={this.abrirModal}>Mostrar Modal</Button>
+      <Modal isOpen={this.state.abierto} style={modalStyles}>
+        <Hoja/>
+        <ModalFooter>
+            <Button color="secondary" onClick={this.abrirModal}>Cerrar</Button>
+        </ModalFooter>
+      </Modal> 
+        
+      </div></div>
+
+        
+      </>
+
+      
+    )
+  }
+}
+
+export default Modal4;
+
