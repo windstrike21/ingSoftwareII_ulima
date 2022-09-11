08392dfa32d3ae014cb656c5828c230483f2cb60
andy <andygianlucca90@hotmail.com>
2022-09-11 14:31:55 -0500

    Rutas7

--- src/Paginas/Personal/RegistroPorPersonal.js
+++ src/Paginas/Personal/RegistroPorPersonal.js
@@ -7,89 +7,89 @@ import Modal4 from "../Personal/modals/mor4";
 function Registro_Personal() {
 
   return (
-    
+
 
     <div className="Registro_Personal mt-5">
 
-<div className="d-grid gap-2 col-6 mx-auto justify-content-center">
+      <div className="d-grid gap-2 col-6 mx-auto justify-content-center">
         <h1>Registro De Afiliación</h1>
       </div>
-      
-   
+
+
       <div className="d-grid gap-2 col-9 mx-auto justify-content-end">
-      <div className="row mt-3 mb-3 ">
-        <Modal1/>
+        <div className="row mt-3 mb-3 ">
+          <Modal1 />
+        </div>
       </div>
+
+
+      <div className="container border 2px mt-3">
+        <table className="table ">
+          <thead>
+            <tr></tr>
+          </thead>
+          <tbody>
+            <tr className="Row">
+              <td>
+                <div className="col">
+                  Codigo usuario
+                </div>
+              </td>
+              <td>
+                <div className="container justify-content-end d-grid gap-2  ">
+
+                  <Modal4 />
+                  <Modal3 />
+                </div>
+              </td>
+            </tr>
+
+            <tr className="Row">
+              <td>
+                <div className="col">
+                  Codigo usuario
+                </div>
+              </td>
+              <td>
+                <div className="container justify-content-end d-grid gap-2  ">
+
+                  <Modal4 />
+                  <Modal3 />
+                </div>
+              </td>
+            </tr>
+
+            <tr className="Row">
+              <td>
+                <div className="col">
+                  Codigo usuario
+                </div>
+              </td>
+              <td>
+                <div className="container justify-content-end d-grid gap-2  ">
+
+                  <Modal4 />
+                  <Modal3 />
+                </div>
+              </td>
+            </tr>
+            <tr className="Row">
+              <td>
+                <div className="col">
+                  Codigo usuario
+                </div>
+              </td>
+              <td>
+                <div className="container justify-content-end d-grid gap-2  ">
+
+                  <Modal4 />
+                  <Modal3 />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-
-
-<div className="container border 2px mt-3">
-<table className="table ">
-    <thead>
-      <tr></tr>
-    </thead>
-    <tbody>
-      <tr className="Row">
-        <td>
-          <div className="col">
-            Codigo usuario 
-          </div>
-        </td>
-        <td> 
-        <div className="container justify-content-end d-grid gap-2  ">
-
-            <Modal4/>
-            <Modal3/>
-             </div> 
-        </td>
-      </tr>
-
-      <tr className="Row">
-        <td>
-          <div className="col">
-            Codigo usuario 
-          </div>
-        </td>
-        <td> 
-        <div className="container justify-content-end d-grid gap-2  ">
-
-            <Modal4/>
-            <Modal3/>
-             </div> 
-        </td>
-      </tr>
-
-      <tr className="Row">
-        <td>
-          <div className="col">
-            Codigo usuario 
-          </div>
-        </td>
-        <td> 
-        <div className="container justify-content-end d-grid gap-2  ">
-
-            <Modal4/>
-            <Modal3/>
-             </div> 
-        </td>
-      </tr>
-      <tr className="Row">
-        <td>
-          <div className="col">
-            Codigo usuario 
-          </div>
-        </td>
-        <td> 
-        <div className="container justify-content-end d-grid gap-2  ">
-
-            <Modal4/>
-            <Modal3/>
-             </div> 
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
 
     </div>
   );
