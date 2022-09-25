c31332c5ea900486839a1bcfe70e742c302d088e
Alec <20193586@aloe.ulima.edu.pe>
2022-09-25 18:01:04 -0500

    Historial Clinico - medico

---
+++ src/Paginas/Medico/AgregarHistoriaM.js
@@ -0,0 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const AgregarHistoriaM = () => {
+    const location=useLocation()
+    return (
+        <div className="container m-4 p-4">
+            
+        <h3>Agregar Historia: {location.state.nombre}</h3>
+        <br></br>
+        <div className="container mb-1">
+            
+            <div className="form-group">
+                <label className="form-label">Fecha: </label>
+                <input type="date" placeholder='Agregue una fecha' className="form-control"></input>
+            </div>
+
+            <div className="form-group">
+                <label className="form-label">Doctor: </label>
+                <input type="text" placeholder='Agregue un doctor' className="form-control"></input>
+            </div>
+
+            <div className="form-group">
+                <label className="form-label">Especialidad: </label>
+                <input type="text" placeholder='Agregue una especialidad' className="form-control"></input>
+            </div>
+
+            <div className="form-group">
+                <label className="form-label">Modificar descripción: </label>
+                <textarea type="text" placeholder='Agregue una descripción' className="form-control"></textarea>
+            </div>
+        </div>
+
+        <br></br>
+
+        <div className="row">
+            <div className="col">
+                <button type="button" className="btn btn-light" >Agregar Historia</button>
+            </div>
+            <div className="col">
+                <button type="button" className="btn btn-light" >Cancelar</button>
+            </div>
+        </div>
+        
+    </div>
+    )
+}
+export default AgregarHistoriaM;
---
+++ src/Paginas/Medico/Buscar pacientes para ver perfilM.js
@@ -0,0 +1,44 @@
+import InicioAlPerfilPaciente from "../../Componentes/Inicio al perfil paciente";
+import React, { useEffect, useState } from "react"
+import Axios from "axios";
+
+const BuscarPacientesParaVerPerfilM = (props) => {
+    const [datosPac, setDatosPac] = useState([])
+    useEffect(() => {
+        Axios.get("https://ulimasoftwareiibackend-production.up.railway.app/usuarios/Paciente")
+            .then(res => {
+                
+                setDatosPac(res.data)
+            }).catch(console.log)
+    }, [])
+    const obtenerPaciente=(event)=>{
+        Axios.get(`https://ulimasoftwareiibackend-production.up.railway.app/usuarios/Paciente/${event.target.value}`)
+        .then(res => {
+            
+            setDatosPac(res.data)
+        }).catch(console.log)
+    }
+    console.log(datosPac)
+    const DatosPac =()=>{
+        return(datosPac)
+    }
+    return (
+        <div>
+            <div className="container mt-3">
+                <div className="row">
+                    <h3 className="col-md-4">Pacientes</h3>
+                    <input onChange={obtenerPaciente} placeholder="Buscar Paciente..." className="col-8"></input>
+                </div>
+
+            </div>
+
+            <div className="container mt-4">
+                {datosPac.map((datos)=>(
+                    <InicioAlPerfilPaciente nombre={datos.nombres} datosPac={DatosPac} setDatosPac={setDatosPac} codigo="002001902" />
+                ))}
+                
+            </div>
+        </div>
+    )
+}
+export default BuscarPacientesParaVerPerfilM;
