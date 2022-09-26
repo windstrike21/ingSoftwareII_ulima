484ec25fb2aa6fec3293c5caebc5660b7753e863
Maurice Frayssinet C <mauricefrayssinet@gmail.com>
2022-09-25 20:19:43 -0500

    chat area js semiacabado

--- src/Componentes/ChatArea.jsx
+++ src/Componentes/ChatArea.jsx
@@ -1,45 +1,74 @@
+import { useState } from "react";
 import img1 from "../Imagenes/Personal/logo_canal_ayuda_tecnica.ico";
 import "../css/personal/ChatArea.css";
 import React from 'react';
 const ChatArea = () => {
+    const [mensaje, setMensaje] = useState("");
+    const [mensajes, setMensajes] = useState([]);
+
+    const actualizarMensaje = (event) => {
+        setMensaje(event.target.value);
+    }
+
+    const enviarDatos = () => {
+        console.log("prueba");
+        if (mensajes.length == 0) {
+            setMensajes([mensaje]);    
+        } else {
+            setMensajes(mensajes => [...mensajes,mensaje]);          
+        }        
+    }
+
     return <div className="row">
         <div className="col mt-1 ps-1 pe-0">
             <div className="overflow-auto componente mb-2 me-4">
-            <table className="table border border-white">
-                <tbody>
-                    <tr className="row me-0">
-                        <td className="col-1">
-                            <div className="py-2 text-end">
-                                <img src={img1} alt="..." className="rounded-5" height="20px" width="20px" />
-                            </div>
-                        </td>
-                        <td className="col-11">
-                            <p className="mb-0 p-2 w-75 rounded-2 bg-light">
-                                Bienvenido al canal de consultas!
-                                Para registrar su consulta escriba en el campo inferior “Escriba su consulta...”
-                                Un administrador se pondrá en contacto con usted en un plazo no mayor a 48 horas.
-                            </p>
-                        </td>
-                    </tr>
-                    <tr>                        
-                        <td>
-                            <p className="mb-0 p-2 w-75 rounded-2 bg-secondary ms-auto text-white">
-                                Hola tengo una consulta...
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                        </td>
-                    </tr>                      
-                </tbody>
-            </table>
+                <table className="table border border-white">
+                    <tbody key="tbody_1">
+                        <tr className="row me-0">
+                            <td className="col-1">
+                                <div className="py-2 text-end">
+                                    <img src={img1} alt="..." className="rounded-5" height="20px" width="20px" />
+                                </div>
+                            </td>
+                            <td className="col-11">
+                                <p className="mb-0 p-2 w-75 rounded-2 bg-light">
+                                    Bienvenido al canal de consultas!
+                                    Para registrar su consulta escriba en el campo inferior “Escriba su consulta...”
+                                    Un administrador se pondrá en contacto con usted en un plazo no mayor a 48 horas.
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="mb-0 p-2 w-75 rounded-2 bg-secondary ms-auto text-white">
+                                    Hola tengo una consulta...
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </td>
+                        </tr>
+                        {
+                            mensajes.map((mensaje) => (
+                                <tr key={mensaje.id}>
+                                    <td>
+                                        <p className="mb-0 p-2 w-75 rounded-2 bg-secondary ms-auto text-white">
+                                            {mensaje}
+                                        </p>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
         <div className="row pe-5">
             <div className="col-md-11 mb-2 ps-4">
-                <textarea className="form-control" placeholder="Ingrese su consulta aquí" rows={3} ></textarea>
+                <textarea key="txtarea_1" className="form-control" placeholder="Ingrese su consulta aquí" rows={3}
+                    onChange={actualizarMensaje}></textarea>
             </div>
             <div className="col-md-1 mb-2 text-center">
-                <button type="button" className="p-2 btn btn-secondary opacity-50">ENVIAR</button>
+                <button type="button" className="p-2 btn btn-secondary opacity-50" onClick={enviarDatos}>ENVIAR</button>
             </div>
         </div>
     </div>
