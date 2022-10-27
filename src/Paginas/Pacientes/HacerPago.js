9c66799aad9da6c2c17a40d6612649bad0348962
Wazonga <rmiglio115@gmail.com>
2022-10-27 18:18:52 -0500

    m

--- src/Paginas/Pacientes/HacerPago.js
+++ src/Paginas/Pacientes/HacerPago.js
@@ -31,13 +31,13 @@ export const MetodoPagoTarjeta = (props) => {
         <div className='container border mt-3 col-4'>
             <FormGroup>
                 <Label for="NumCard">Numero de Tarjeta</Label>
-                <Input type="text" onChange={handleChange} name="numcard" value={props.pacBody.numcard}/> 
+                <Input type="text" onChange={handleChange} name="numcard" value={props.TarjetaBody.numcard}/> 
                 <Label className="col-1 col-md-3" >Fecha de Vencimiento </Label>
-                <Input className="col-5 col-mx-2" onChange={handleChange} name="fecven" value={props.pacBody.fecven}></Input>
+                <Input className="col-5 col-mx-2" onChange={handleChange} name="fecven" value={props.TarjetaBody.fecven}></Input>
                 <Label className="col-4 col-md-1">CVV </Label>
-                <Input className="col-3 "  onChange={handleChange} name="cvv" value={props.pacBody.cvv}></Input>
+                <Input className="col-3 "  onChange={handleChange} name="cvv" value={props.TarjetaBody.cvv}></Input>
                 <Label for="Name">Nombre del Propietario</Label>
-                <Input type="text"  onChange={handleChange} name="nombre" value={props.pacBody.nombre}/> 
+                <Input type="text"  onChange={handleChange} name="nombre" value={props.TarjetaBody.nombre}/> 
             </FormGroup>
 
             <div className="my-3 d-flex justify-content-center">
