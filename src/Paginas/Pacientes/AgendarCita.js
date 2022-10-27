d101221ed3cf98c920c0e85316f8d6d362ed0587
Jimbravo <jimbravo05@hotmail.com>
2022-10-27 16:39:38 -0500

    fix

--- src/Paginas/Pacientes/AgendarCita.js
+++ src/Paginas/Pacientes/AgendarCita.js
@@ -9,14 +9,14 @@ const AgendarCita = () => {
     return(
         <body>
             <div>
-                <MenuDesp></MenuDesp>
-                <div class = "container rounded-4" id ="container1">
-                    <div class = "col d-lg-block col-md-5">
-                        <p class=" text-center">
-                                perfil clinico
-                        </p>
-                    </div>
-                </div>
+                <div class = "cajita rounded-4">
+                        <div class="row">
+                            <h2 class ="texto">Agendar una cita</h2>
+                            <div class ="datosdiag col-5 rounded-3">
+                            </div>
+                                <button class="botoncito rounded-1">COMPRAR </button>
+                        </div>
+                </div>      
             </div>
         </body>
     );
