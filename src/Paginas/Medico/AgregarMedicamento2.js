a998b834ae8ce28bd9d5438d627d7d5f0f820154
Alec <20193586@aloe.ulima.edu.pe>
2022-11-27 12:19:27 -0500

    back medicamentos y editar datos paciente

--- src/Paginas/Medico/AgregarMedicamento2.js
+++ src/Paginas/Medico/AgregarMedicamento2.js
@@ -1,7 +1,33 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css'
+import React, { useState } from 'react';
 
-export default function AgregarMedicamento2() {
+const AgregarMedicamento2 = () => {
+    const [med, setMed]=useState({
+        medicamento: null,
+        frecuencia: null,
+        duracion: null,
+        unidades: null
+    })
+
+    const actualizarDatos = (event) => {
+        const { name, value } = event.target
+        setMed(prevBody => ({
+            ...prevBody,
+            [name]: value
+
+        }))
+    }
+
+    const agregar = () => {
+        Axios.post(`http://localhost:5000/citaMedicamentos`, {
+           medicamento:med.medicamento,
+           frecuencia:med.frecuencia,
+           duracion:med.duracion,
+           unidades:med.unidades
+        })
+    }
+
     return (
         <div className="container m-4 p-4">
             
@@ -11,22 +37,22 @@ export default function AgregarMedicamento2() {
             
             <div className="form-group">
                 <label className="form-label">Medicamento: </label>
-                <input type="text" placeholder='Agregue un medicamento' className="form-control" name="medicamento"></input>
+                <input type="text" placeholder='Agregue un medicamento' className="form-control" name="medicamento" value={med.medicamento} onChange={actualizarDatos}></input>
             </div>
 
             <div className="form-group">
                 <label className="form-label">Cantidad: </label>
-                <input type="text" placeholder='Agregue la cantidad' className="form-control" name="cant"></input>
+                <input type="text" placeholder='Agregue la cantidad' className="form-control" name="cant" value={med.unidades} onChange={actualizarDatos}></input>
             </div>
 
             <div className="form-group">
                 <label className="form-label">Duración: </label>
-                <input type="text" placeholder='Agregue la duración' className="form-control" name="duracion"></input>
+                <input type="text" placeholder='Agregue la duración' className="form-control" name="duracion" value={med.duracion} onChange={actualizarDatos}></input>
             </div>
 
             <div className="form-group">
                 <label className="form-label">Frecuencia: </label>
-                <input type="text" placeholder='Agregue la frecuencia' className="form-control" name="frec"></input>
+                <input type="text" placeholder='Agregue la frecuencia' className="form-control" name="frec" value={med.frecuencia} onChange={actualizarDatos}></input>
             </div>
         </div>
 
@@ -34,7 +60,7 @@ export default function AgregarMedicamento2() {
 
         <div className="row">
             <div className="col">
-                <button type="button" className="btn btn-light">Agregar Medicamento</button>
+                <button type="button" className="btn btn-light" onClick={agregar}>Agregar Medicamento</button>
             </div>
             <div className="col">
                 <button type="button" className="btn btn-light" >Cancelar</button>
@@ -44,3 +70,4 @@ export default function AgregarMedicamento2() {
     </div>
     )
 }
+export default AgregarMedicamento2;
