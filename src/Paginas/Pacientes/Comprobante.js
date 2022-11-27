f740bf452db87e4ca30461717fef94f812e47a3d
Wazonga <rmiglio115@gmail.com>
2022-11-27 13:07:06 -0500

    avance

--- src/Paginas/Pacientes/Comprobante.js
+++ src/Paginas/Pacientes/Comprobante.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { FormGroup, Label } from 'reactstrap';
+import { FormGroup, Input, Label } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const MostrarBoleta = () =>{
+export const MostrarBoleta = () =>{
     
 
     return (
         <div className=' container  mt-3 col-6'>
-           <h1>HOLA</h1>
+            <FormGroup>
+                <Label for="NumCard">Numero de Tarjeta</Label>
+                <Label type="text" name="numcard" />
+
+                <Label className="col-1 col-md-5" >Clinica  #### </Label>
+                <Label className="col-5 col-mx-2" name="nombre"/>
+
+                <Label for="Nombre">Nombre de Paciente</Label>
+                <Label type="text" name="nombre" />
+
+                <Label for="Monto">Adquisición </Label>
+                <Label type="text" name="Monto"/>
+            </FormGroup>
         </div>
     )
 
